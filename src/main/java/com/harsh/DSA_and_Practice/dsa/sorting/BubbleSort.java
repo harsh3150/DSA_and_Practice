506de5e1dc6351112379991685d89e11a4fe929f
Harsh <harshvardhan3150@gmail.com>
2022-10-11 00:09:24 +0530

    bubble sorting

--- src/main/java/com/harsh/DSA_and_Practice/dsa/sorting/BubbleSort.java
+++ src/main/java/com/harsh/DSA_and_Practice/dsa/sorting/BubbleSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         System.out.println("Before sorting:");
-        int arr[] = {2, 4, 1, 0, 23, 3};
+        int arr[] = {4, 5, 9, 12, 9, 22, 45, 7};
         System.out.println(Arrays.toString(arr));
         bubbleSort(arr);
         System.out.println("After sorting:");
@@ -14,5 +14,20 @@ public class BubbleSort {
 
     public static void bubbleSort(int []arr){
 
+        for(int i = 0; i<arr.length; i++){
+
+            for(int j = 1; j<arr.length-i; j++){
+                if(arr[j-1] > arr[j]){
+                    swap(j-1, j, arr);
+                }
+            }
+
+        }
+    }
+
+    private static void swap(int i, int j, int []arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
