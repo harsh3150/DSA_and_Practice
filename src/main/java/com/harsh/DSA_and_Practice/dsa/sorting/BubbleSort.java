c7ca32a6c0c0d9853ad4d98b879de826860e9bd3
Harsh <harshvardhan3150@gmail.com>
2022-10-11 00:19:36 +0530

    Cleanup

--- src/main/java/com/harsh/DSA_and_Practice/dsa/sorting/BubbleSort.java
+++ src/main/java/com/harsh/DSA_and_Practice/dsa/sorting/BubbleSort.java
@@ -13,15 +13,12 @@ public class BubbleSort {
     }
 
     public static void bubbleSort(int []arr){
-
         for(int i = 0; i<arr.length; i++){
-
             for(int j = 1; j<arr.length-i; j++){
                 if(arr[j-1] > arr[j]){
                     swap(j-1, j, arr);
                 }
             }
-
         }
     }
 
